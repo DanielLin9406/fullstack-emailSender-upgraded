c148a07dd79eb0094fef971e2872c2795bc71a6c
Daniel Lin <daniel.lin@positivegrid.com>
2019-04-19 14:57:57 +0800

    Add

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const http = require('http');
-const appInstance = require(path.resolve(__dirname, 'src', 'app'));
+const appInstance = require(path.resolve(__dirname, 'src')+'/app.js');
 
 // Launch http server
  http.createServer(appInstance);
